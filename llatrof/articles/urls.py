24927033266063effbfc0b5977f5f3e3ee0eb4d4
JAY-Winter <rubat0@naver.com>
2022-11-29 15:20:52 +0900

    feat: DRF 전환

--- llatrof/articles/urls.py
+++ llatrof/articles/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'articles'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('category/', views.category, name='category'),
-    path('category/<str:category>/', views.category_goods, name='category_goods'),
-    path('brand/', views.brand, name='brand'),
-    path('brand/<str:brand>/', views.brand_goods, name='category_brand'),
-    path('api/articles_list/', views.articles_list, name='articles_list'),
+    path('', views.articles_list),
+    path('category/', views.category),
+    path('category/<str:category>/', views.category_goods),
+    path('brand/', views.brand),
+    path('brand/<str:brand>/', views.brand_goods),
 ]
