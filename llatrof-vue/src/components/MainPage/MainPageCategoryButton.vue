bf0f2e59051ada68a8a71001198be665dcd09c05
JAY-Winter <rubat0@naver.com>
2022-12-16 00:43:15 +0900

    feat: MainPage Category Button 디자인

--- llatrof-vue/src/components/MainPage/MainPageCategoryButton.vue
+++ llatrof-vue/src/components/MainPage/MainPageCategoryButton.vue
@@ -1,58 +1,69 @@
 <template>
-  <div class="row row-cols-5">
-    <div class="col">
+  <div class="row row-cols-3">
+    <div>
       <router-link
-        @click.native="closeAside"
-        :to="{ name: 'category', params: { categoryName: '데님' } }"
-        ><div id="div_size">데님</div></router-link
+      @click.native="closeAside"
+      :to="{ name: 'category', params: { categoryName: '데님' } }"
+      ><div id="inner_div">DENIM</div></router-link
       >
-    </div>
-    <div class="col">
-      <router-link
+      </div>
+      <div>
+        <router-link
         @click.native="closeAside"
         :to="{ name: 'category', params: { categoryName: '코튼' } }"
-        ><div id="div_size">코튼</div></router-link
+        ><div id="inner_div">COTTON</div></router-link
       >
-    </div>
-    <div class="col">
-      <router-link
+      </div>
+      <div>
+        <router-link
         @click.native="closeAside"
         :to="{ name: 'category', params: { categoryName: '슈트' } }"
-        ><div id="div_size">슈트</div></router-link
-      >
-    </div>
-    <div class="col">
-      <router-link
+        ><div id="inner_div">SLACKS</div></router-link
+        >
+        </div>
+      <div>
+        <router-link
         @click.native="closeAside"
         :to="{ name: 'category', params: { categoryName: '트레이닝' } }"
-        ><div id="div_size">트레이닝</div></router-link
-      >
-    </div>
-    <div class="col">
-      <router-link
+        ><div id="inner_div">TRAINING</div></router-link
+        >
+      </div>
+      <div>
+        <router-link
+        @click.native="closeAside"
+        :to="{ name: 'category', params: { categoryName: '숏' } }"
+        ><div id="inner_div">SHORTS</div></router-link
+        >
+      </div>
+      <div>
+        <router-link
         @click.native="closeAside"
         :to="{ name: 'category', params: { categoryName: '기타' } }"
-        ><div id="div_size">기타</div></router-link
-      >
+        ><div id="inner_div">ETC</div></router-link
+        >
     </div>
   </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
   name: "MainPageCategorydiv",
   props: {
-    categoryInfo: Object,
+  categoryInfo: Object,
   },
-};
-</script>
-
-<style>
-#div_size {
+  };
+  </script>
+  
+  <style>
+  #inner_div{
   color: black;
-  border-radius: 10%;
-  text-align: center;
-  border: 1px black solid;
-  margin: 3 0 0 0;
-}
-</style>
+  background-color: rgba(169, 165, 165, 0.2);
+  margin: 5px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  border-radius: 5px;
+  justify-content: center;
+  font-size: 1.1rem;
+  }
+  </style>
